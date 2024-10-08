42a164319f6e6e587679a092fa2730a4c2762648
ozkzrl <ozkan.zorlu@gmail.com>
2024-10-08 18:15:43 +0200

    ikinci

--- obj/Debug/net6.0/WebApplication10.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplication10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8fd4ea81f59e85899911574633bd6434c7b388")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
