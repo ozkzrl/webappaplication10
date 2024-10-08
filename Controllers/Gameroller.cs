23863390d15787b0c8f318d2e62d317b4e238835
ozkzrl <ozkan.zorlu@gmail.com>
2024-10-08 18:31:16 +0200

    third commit

---
+++ Controllers/Gameroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class Gameroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
